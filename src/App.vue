a65c98ca063d6c1ea84cbdf9d490830f4b9922fa
Nimda <39852038+onurrozkaan@users.noreply.github.com>
2018-11-27 22:27:09 +0300

    optimization

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-
   <div id="app">
-    <h1>A</h1>
+    <div class="col-12 col-md-6">
+      <div class="stock-news-area">
+        <div id="stockNewsTicker" class="ticker">
+          <ul>
+            <li>
+              <div class="single-stock-report">
+                <navbarCurrency coinName="BTC" moneyType="USD"/>
+                <differentOfCurrency coinName="BTC" moneyType="USD"/>
+              </div>
+
+              <div class="single-stock-report">
+                <navbarCurrency coinName="ETH" moneyType="USD"/>
+                <differentOfCurrency coinName="ETH" moneyType="USD"/>
+              </div>
+
+              <div class="single-stock-report">
+                <navbarCurrency coinName="XRP" moneyType="USD"/>
+                <differentOfCurrency coinName="XRP" moneyType="USD"/>
+              </div>
+            </li>
+            <li>
+              <div class="single-stock-report">
+                <navbarCurrency coinName="BTC" moneyType="TRY"/>
+                <differentOfCurrency coinName="BTC" moneyType="TRY"/>
+              </div>
+
+              <div class="single-stock-report">
+                <navbarCurrency coinName="ETH" moneyType="TRY"/>
+                <differentOfCurrency coinName="ETH" moneyType="TRY"/>
+              </div>
+
+              <div class="single-stock-report">
+                <navbarCurrency coinName="XRP" moneyType="TRY"/>
+                <differentOfCurrency coinName="XRP" moneyType="TRY"/>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-  }
-}
+  name: "app",
+  components: {}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
