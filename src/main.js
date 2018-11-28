8879719044ddf22767c36e21b91c7e8322168b3f
Nimda <39852038+onurrozkaan@users.noreply.github.com>
2018-11-28 19:56:20 +0300

    build name updated

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import cryptobar from "./cryptobar.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(cryptobar)
+}).$mount("#app");
