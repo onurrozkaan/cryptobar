8af0dffd75e8fad23a7d65dd42de9f4b75013e72
Nimda <39852038+onurrozkaan@users.noreply.github.com>
2018-11-28 12:38:01 +0300

    run js has been done

--- public/js/run.js
+++ public/js/run.js
@@ -1,1 +1,19 @@
-// RUN JS
+(function($) {
+  "use strict";
+
+  // Newsticker Active Code
+  $.simpleTicker($("#breakingNewsTicker"), {
+    speed: 1000,
+    delay: 3500,
+    easing: "swing",
+    effectType: "roll"
+  });
+
+  // Newsticker Active Code
+  $.simpleTicker($("#stockNewsTicker"), {
+    speed: 1200,
+    delay: 3500,
+    easing: "swing",
+    effectType: "roll"
+  });
+})(jQuery);
