36201aae46fbe8abd063093e53b6209635540f34
Nimda <39852038+onurrozkaan@users.noreply.github.com>
2018-11-28 19:56:49 +0300

    optimization

--- src/cryptobar.vue
+++ src/cryptobar.vue
@@ -1,48 +1,3 @@
-<template>
-  <div id="app" style="background-color:#100b26!important;">
-    <div class="col-12 col-md-6">
-      <div class="stock-news-area">
-        <div id="stockNewsTicker" class="ticker">
-          <ul>
-            <li>
-              <div class="single-stock-report">
-                <navbarCurrency coinName="BTC" moneyType="USD"/>
-                <differentOfCurrency coinName="BTC" moneyType="USD"/>
-              </div>
-
-              <div class="single-stock-report">
-                <navbarCurrency coinName="ETH" moneyType="USD"/>
-                <differentOfCurrency coinName="ETH" moneyType="USD"/>
-              </div>
-
-              <div class="single-stock-report">
-                <navbarCurrency coinName="XRP" moneyType="USD"/>
-                <differentOfCurrency coinName="XRP" moneyType="USD"/>
-              </div>
-            </li>
-            <li>
-              <div class="single-stock-report">
-                <navbarCurrency coinName="BTC" moneyType="TRY"/>
-                <differentOfCurrency coinName="BTC" moneyType="TRY"/>
-              </div>
-
-              <div class="single-stock-report">
-                <navbarCurrency coinName="ETH" moneyType="TRY"/>
-                <differentOfCurrency coinName="ETH" moneyType="TRY"/>
-              </div>
-
-              <div class="single-stock-report">
-                <navbarCurrency coinName="XRP" moneyType="TRY"/>
-                <differentOfCurrency coinName="XRP" moneyType="TRY"/>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script>
 import navbarCurrency from "./components/navbarCurrency.vue";
 import differentOfCurrency from "./components/differentOfCurrency.vue";
@@ -146,7 +101,7 @@ export default {
     }
     refreshFunction();
   },
-  name: "app",
+  name: "cryptobar",
   props: {
     //
   },
@@ -157,3 +112,60 @@ export default {
   }
 };
 </script>
+<template>
+  <header class="header-area" id="app">
+    <div class="top-header">
+      <div class="container h-100">
+        <div class="row h-100 align-items-center">
+          <!-- Breaking News Area -->
+          <div class="col-12 col-md-6">
+            <div class="breaking-news-area">
+              <div id="breakingNewsTicker" class="ticker"></div>
+            </div>
+          </div>
+          <!-- Stock News Area -->
+          <div class="col-12 col-md-6">
+            <div class="stock-news-area">
+              <div id="stockNewsTicker" class="ticker">
+                <ul>
+                  <li>
+                    <div class="single-stock-report">
+                      <navbarCurrency coinName="BTC" moneyType="USD"/>
+                      <differentOfCurrency coinName="BTC" moneyType="USD"/>
+                    </div>
+
+                    <div class="single-stock-report">
+                      <navbarCurrency coinName="ETH" moneyType="USD"/>
+                      <differentOfCurrency coinName="ETH" moneyType="USD"/>
+                    </div>
+
+                    <div class="single-stock-report">
+                      <navbarCurrency coinName="XRP" moneyType="USD"/>
+                      <differentOfCurrency coinName="XRP" moneyType="USD"/>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="single-stock-report">
+                      <navbarCurrency coinName="BTC" moneyType="TRY"/>
+                      <differentOfCurrency coinName="BTC" moneyType="TRY"/>
+                    </div>
+
+                    <div class="single-stock-report">
+                      <navbarCurrency coinName="ETH" moneyType="TRY"/>
+                      <differentOfCurrency coinName="ETH" moneyType="TRY"/>
+                    </div>
+
+                    <div class="single-stock-report">
+                      <navbarCurrency coinName="XRP" moneyType="TRY"/>
+                      <differentOfCurrency coinName="XRP" moneyType="TRY"/>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
