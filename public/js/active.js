062511414e14b5a78935551c3bd47af52b6e0cf1
Nimda <39852038+onurrozkaan@users.noreply.github.com>
2018-12-03 23:33:54 +0300

    optimization

--- public/js/active.js
+++ public/js/active.js
@@ -1,151 +1,136 @@
 /* eslint-disable no-undef */
-(function ($) {
-    'use strict';
-
-    var $window = $(window);
-
-    // Fullscreen Active Code
-    $window.on('resizeEnd', function () {
-        $(".full_height").height($window.height());
+!(function(e) {
+  (e.simpleTicker = function(t, i) {
+    var s = { speed: 1e3, delay: 3e3, easing: "swing", effectType: "slide" },
+      n = {
+        ul: "",
+        li: "",
+        initList: "",
+        ulWidth: "",
+        liHeight: "",
+        tickerHook: "tickerHook",
+        effect: {}
+      },
+      c = this;
+    c.settings = {};
+    e(t), (this.t = t);
+    (c.init = function() {
+      switch (
+        ((c.settings = e.extend({}, s, i)),
+        (n.ul = t.children("ul")),
+        (n.li = t.find("li")),
+        (n.initList = t.find("li:first")),
+        (n.ulWidth = n.ul.width()),
+        (n.liHeight = n.li.height()),
+        t.css({ height: n.liHeight }),
+        n.li.css({ top: "0", left: "0", position: "absolute" }),
+        c.settings.effectType)
+      ) {
+        case "fade":
+          c.effect.fade();
+          break;
+        case "roll":
+          c.effect.roll();
+          break;
+        case "slide":
+          c.effect.slide();
+      }
+      c.effect.exec();
+    }),
+      (c.effect = {}),
+      (c.effect.exec = function() {
+        n.initList
+          .css(n.effect.init.css)
+          .animate(n.effect.init.animate, c.settings.speed, c.settings.easing)
+          .addClass(n.tickerHook),
+          t.find(n.li).length > 1 &&
+            setInterval(function() {
+              t.find("." + n.tickerHook)
+                .animate(
+                  n.effect.start.animate,
+                  c.settings.speed,
+                  c.settings.easing
+                )
+                .next()
+                .css(n.effect.next.css)
+                .animate(
+                  n.effect.next.animate,
+                  c.settings.speed,
+                  c.settings.easing
+                )
+                .addClass(n.tickerHook)
+                .end()
+                .appendTo(n.ul)
+                .css(n.effect.end.css)
+                .removeClass(n.tickerHook);
+            }, c.settings.delay);
+      }),
+      (c.effect.fade = function() {
+        n.effect = {
+          init: {
+            css: { display: "block", opacity: "0" },
+            animate: { opacity: "1", zIndex: "98" }
+          },
+          start: { animate: { opacity: "0" } },
+          next: {
+            css: { display: "block", opacity: "0", zIndex: "99" },
+            animate: { opacity: "1" }
+          },
+          end: { css: { display: "none", zIndex: "98" } }
+        };
+      }),
+      (c.effect.roll = function() {
+        n.effect = {
+          init: {
+            css: { top: "3em", display: "block", opacity: "0" },
+            animate: { top: "0", opacity: "1", zIndex: "98" }
+          },
+          start: { animate: { top: "-3em", opacity: "0" } },
+          next: {
+            css: { top: "3em", display: "block", opacity: "0", zIndex: "99" },
+            animate: { top: "0", opacity: "1" }
+          },
+          end: { css: { zIndex: "98" } }
+        };
+      }),
+      (c.effect.slide = function() {
+        n.effect = {
+          init: {
+            css: { left: 200, display: "block", opacity: "0" },
+            animate: { left: "0", opacity: "1", zIndex: "98" }
+          },
+          start: { animate: { left: -200, opacity: "0" } },
+          next: {
+            css: {
+              left: n.ulWidth,
+              display: "block",
+              opacity: "0",
+              zIndex: "99"
+            },
+            animate: { left: "0", opacity: "1" }
+          },
+          end: { css: { zIndex: "98" } }
+        };
+      }),
+      c.init();
+  }),
+    (e.fn.simpleTicker = function(t) {
+      return this.each(function() {
+        if (void 0 == e(this).data("simpleTicker")) {
+          var i = new e.simpleTiecker(this, t);
+          e(this).data("simpleTicker", i);
+        }
+      });
     });
+})(jQuery);
+(function($) {
+  "use strict";
 
-    $window.on('resize', function () {
-        if (this.resizeTO) clearTimeout(this.resizeTO);
-        this.resizeTO = setTimeout(function () {
-            $(this).trigger('resizeEnd');
-        }, 300);
-    }).trigger("resize");
-
-    // Newsticker Active Code
-    $.simpleTicker($("#breakingNewsTicker"), {
-        speed: 1000,
-        delay: 3500,
-        easing: 'swing',
-        effectType: 'roll'
-    });
-
-    // Newsticker Active Code
-    $.simpleTicker($("#stockNewsTicker"), {
-        speed: 1200,
-        delay: 3500,
-        easing: 'swing',
-        effectType: 'roll'
-    });
-
-    var welcomeSlide = $('.welcome-blog-post-slide');
-
-    // Welcome Slider Active Code
-    if ($.fn.owlCarousel) {
-        welcomeSlide.owlCarousel({
-            items: 3,
-            margin: 0,
-            loop: true,
-            nav: false,
-            dots: true,
-            autoplay: true,
-            autoplayTimeout: 5000,
-            smartSpeed: 1000,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                576: {
-                    items: 2
-                },
-                992: {
-                    items: 3
-                }
-            }
-        });
-    }
-    
-    welcomeSlide.on('translate.owl.carousel', function () {
-        var slideLayer = $("[data-animation]");
-        slideLayer.each(function () {
-            var anim_name = $(this).data('animation');
-            $(this).removeClass('animated ' + anim_name).css('opacity', '0');
-        });
-    });
-
-    welcomeSlide.on('translated.owl.carousel', function () {
-        var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
-        slideLayer.each(function () {
-            var anim_name = $(this).data('animation');
-            $(this).addClass('animated ' + anim_name).css('opacity', '1');
-        });
-    });
-
-    $("[data-delay]").each(function () {
-        var anim_del = $(this).data('delay');
-        $(this).css('animation-delay', anim_del);
-    });
-
-    $("[data-duration]").each(function () {
-        var anim_dur = $(this).data('duration');
-        $(this).css('animation-duration', anim_dur);
-    });
-    
-    // Marquee Active Code
-    if ($.fn.SimpleMarquee) {
-        $('.simple-marquee-container').SimpleMarquee({
-            duration: 60000,
-            padding: 0,
-            marquee_class: '.marquee',
-            container_class: '.simple-marquee-container',
-            sibling_class: 0,
-            hover: true
-        });
-    }
-    
-    
-    // Editorial Post Slides
-    if ($.fn.owlCarousel) {
-        $('.editorial-post-slides').owlCarousel({
-            items: 1,
-            margin: 0,
-            loop: true,
-            nav: false,
-            dots: true,
-            autoplay: true,
-            autoplayTimeout: 5000,
-            smartSpeed: 1000
-        });
-    }
-
-    // Search Btn Active Code
-    $('#searchbtn').on('click', function () {
-        $('body').toggleClass('search-form-on');
-    })
-
-    // Video Active Code
-    if ($.fn.magnificPopup) {
-        $('.videobtn').magnificPopup({
-            disableOn: 0,
-            type: 'iframe',
-            mainClass: 'mfp-fade',
-            removalDelay: 160,
-            preloader: true,
-            fixedContentPos: false
-        });
-    }
-
-    // ScrollUp Active Code
-    if ($.fn.scrollUp) {
-        $.scrollUp({
-            scrollSpeed: 1000,
-            easingType: 'easeInOutQuart',
-            scrollText: '<i class="fa fa-angle-up" aria-hidden="true"></i>'
-        });
-    }
-
-    // PreventDefault a Click
-    $("a[href='#']").on('click', function ($) {
-        $.preventDefault();
-    });
-
-    // wow Active Code
-    if ($window.width() > 767) {
-        new WOW().init();
-    }
-
+  // Newsticker Active Code
+  $.simpleTicker($("#stockNewsTicker"), {
+    speed: 1200,
+    delay: 3500,
+    easing: "swing",
+    effectType: "roll"
+  });
 })(jQuery);
